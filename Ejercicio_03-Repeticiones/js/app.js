3fbd41999e9fbf4c348f6380d27cd3e8dcc605a9
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-03-03 09:28:57 -0300

    feat:16. ng-repeat : Parte 2

--- Ejercicio_03-Repeticiones/js/app.js
+++ Ejercicio_03-Repeticiones/js/app.js
@@ -4,5 +4,23 @@
     app.controller("listadoCtrl", ["$scope", function($scope){
 
         $scope.listado = ['Fernando Herrera', 'Melissa Flores', 'Juan Carlos Pineda'];
+        $scope.listadoProfesores = [
+            {
+                nombre: 'Fernando Herrera',
+                edad: 29,
+                clase: "PEE"
+            },
+            {
+                nombre: 'Melissa Flores',
+                edad: 24,
+                clase: 'ICE'
+            },
+            {
+                nombre: 'Juan Carlos Pineda',
+                edad: 42,
+                clase: 'M110'
+            }
+        ];
+        $scope.numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     }]);
 })();
