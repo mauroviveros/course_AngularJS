52fd91d35ff4dede56780c53a83876b8d6ab9dc2
Mauro Daniel Viveros (TRY Ubbe) <maurod.viveros@gmail.com>
2022-03-04 11:36:12 -0300

    feat:34. Tabla dinamica parte 2

--- Ejercicio_10/controllers/alumnos.controller.js
+++ Ejercicio_10/controllers/alumnos.controller.js
@@ -1,8 +1,25 @@
 angular.module("universidadApp").controller("alumnosCtrl", ["$scope", "$http", function($scope, $http){
     $scope.alumnos = [];
+    $scope.position = 5;
 
     $http.get('http://localhost:4000/api/alumnos').then(function(response){
         $scope.alumnos = response.data.data;
     });
     
+    $scope.prevPage = function(){ $scope.position = $scope.position - 5; };
+    $scope.nextPage = function(){
+        console.log("asdsad");
+        $scope.position = $scope.position + 5;
+    };
+
+    $scope.hasDisabled = function(direction){
+        let condition;
+
+        switch(direction){
+            case 'prev': condition = ($scope.position - 5) > 0; break;
+            case 'next': condition = ($scope.position + 5) <= $scope.alumnos.length; break;
+        };
+
+        return !condition;
+    };
 }]);
